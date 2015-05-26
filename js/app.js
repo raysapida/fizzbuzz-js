245c4d58accb828303dd90fe0850fdef5a067af1
Raymond Sapida <ray.sapida@gmail.com>
2015-05-25 17:52:36 -0700

    Add a fizzbuzz named function
    - Changing the form input type to number fullfills the condition of validation
      for an integer instead of text or decimal.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
   $( "#item" ).submit(function( event ) {
-    var x = $("input").val();
+    var input = +$("input").val();
 
-    for(i=1; i < x; i++){
-      if ( (i % 3 == 0) && (i % 5 == 0)){
-        $('body').append('<p>FizzBuzz</p>');
-      } else if ( i % 3 == 0) {
-        $('body').append('<p>Fizz</p>');
-      } else if ( i % 5 == 0) {
-        $('body').append('<p>Buzz</p>');
-      } else {
-        $('body').append('<p>'+i+'</p>');
-      };
-    };
+    fizzbuzz(input);
 
     event.preventDefault();
   });
 
 });
+
+function fizzbuzz(max_number) {
+  for(i=1; i < (max_number+1); i++){
+    if ( (i % 3 == 0) && (i % 5 == 0)){
+      $('body').append('<p>FizzBuzz</p>');
+    } else if ( i % 3 == 0) {
+      $('body').append('<p>Fizz</p>');
+    } else if ( i % 5 == 0) {
+      $('body').append('<p>Buzz</p>');
+    } else {
+      $('body').append('<p>'+i+'</p>');
+    };
+  };
+}
